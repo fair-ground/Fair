f37a9f487184cb480e9d6b7e4852c734f47b7b41
marcprux <mwp1@cornell.edu>
2021-09-26 17:25:27 -0400

    chore: checkpoint

--- Sources/FairCore/FairNet.swift
+++ Sources/FairCore/FairNet.swift
@@ -171,7 +171,7 @@ public extension URLSession {
 
     /// Downloads the given URL request in the current session
     func downloadSync(_ request: URLRequest, timeout: DispatchTime = .distantFuture) throws -> (url: URL, response: URLResponse) {
-        try sync(request: request, timeout: timeout, createTask: downloadTask)
+        try sync(request: request, timeout: timeout, createTask: downloadTaskCopy)
     }
 
     /// Initiates the given task (either `dataTask` or `downloadTask`) and waits for completion.
@@ -197,6 +197,34 @@ public extension URLSession {
             }
         case .timedOut:
             throw GraphQLError(message: "Request timed out")
+        }
+    }
+}
+
+extension URLSession {
+    /// If the download from `downloadTask` is successful, the completion handler receives a URL indicating the location of the downloaded file on the local filesystem. This storage is temporary. To preserve the file, this will move it from the temporary location before returning from the completion handler.
+    /// In practice, macOS seems to be inconsistent in when it ever cleans up these files, so a failure here will manifest itself in occasional missing files.
+    /// This is needed for running an async operation that will still have access to the resulting file.
+    @objc func downloadTaskCopy(with request: URLRequest, completionHandler: @escaping (URL?, URLResponse?, Error?) -> Void) -> URLSessionDownloadTask {
+        self.downloadTask(with: request) { url, response, error in
+            /// Files are generally placed somewhere like: file:///var/folders/24/8k48jl6d249_n_qfxwsl6xvm0000gn/T/CFNetworkDownload_q0k6gM.tmp
+            do {
+                /// We'll copy it to a temporary replaxcement directory with the base name matching the URL's name
+                if let temporaryLocalURL = url,
+                   temporaryLocalURL.isFileURL {
+                   let tempDir = try FileManager.default.url(for: .itemReplacementDirectory, in: .userDomainMask, appropriateFor: temporaryLocalURL, create: true)
+                    let destinationURL = tempDir.appendingPathComponent(temporaryLocalURL.lastPathComponent)
+                    try FileManager.default.moveItem(at: temporaryLocalURL, to: destinationURL)
+                    dbg("replace download file for:", response?.url, "local:", temporaryLocalURL.path, "moved:", destinationURL.path)
+                    return completionHandler(destinationURL, response, error)
+                       
+                }
+            } catch {
+                dbg("ignoring file move error and falling back to un-copied file:", error)
+            }
+
+            // fall-back to the completion handler
+            return completionHandler(url, response, error)
         }
     }
 }
