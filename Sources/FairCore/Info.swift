bdddd0e4c21e477fb2ecd8948a58a60a8cfe91b3
marcprux <mwp1@cornell.edu>
2022-09-22 20:28:42 -0400

    release: 0.6.2

--- Sources/FairCore/Info.swift
+++ Sources/FairCore/Info.swift
@@ -4,6 +4,6 @@ let Info : [String : Any] = [
         "CFBundleIdentifier" : "org.fair-ground.Fair",
         "CFBundleInfoDictionaryVersion" : "6.0",
         "CFBundlePackageType" : "FMWK",
-        "CFBundleShortVersionString" : "0.6.1",
+        "CFBundleShortVersionString" : "0.6.2",
         "NSHumanReadableCopyright" : "GNU Affero General Public License",
     ]
