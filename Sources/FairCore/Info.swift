23c9705f7a99fc41370bdec9a7ef9fbf4c930022
marcprux <marcprux@users.noreply.github.com>
2022-11-17 17:13:33 +0000

    deploy: cacdb831c8666c821032fbfab48c7a756936692d

--- Sources/FairCore/Info.swift
+++ Sources/FairCore/Info.swift
@@ -4,6 +4,6 @@ let Info : [String : Any] = [
         "CFBundleIdentifier" : "org.fair-ground.Fair",
         "CFBundleInfoDictionaryVersion" : "6.0",
         "CFBundlePackageType" : "FMWK",
-        "CFBundleShortVersionString" : "0.7.22",
+        "CFBundleShortVersionString" : "0.7.23",
         "NSHumanReadableCopyright" : "GNU Affero General Public License",
     ]
